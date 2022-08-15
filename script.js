45761a18a50e39a0b99633b045cade7edb82fa7b
Shokha Developer <shokhaakramov@gmail.com>
2022-08-16 01:25:12 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+
+// function maxName(name1, name2, name3) {
+//     let longestName = ""
+//     let arr = [name1, name2, name3]
+//     for (let item of arr) {
+//         if (item.length > longestName.length) {
+//             longestName = item
+//         }
+//     }
+//     console.log(longestName);
+// }
+
+// maxName("Alex", "George", "Michael")
+
+
+
+
+// function min(number1, number2) {
+//     let minNum = 3
+//     let arr = [number1, number2]
+//     for (let item of arr) {
+//         if (item < minNum) {
+//             minNum = item
+//             console.log(arr);
+//         }
+        
+//     }
+// }
+
+// min(1, 2)
+// min([1, 2])
+// min({ a: 1, b: 2 })
+
+
+
+// function bubbleSort(arr) {
+//     for (let i = arr.length - 1; i > 0; i--) {
+//         for (let k = 0; k < i; k++) {
+//             if (arr[k] > arr[k + 1]) {
+//                 let temp = arr[k];
+//                 arr[k] = arr[k + 1];
+//                 arr[k + 1] = temp;
+//             }
+//         }
+//     }
+//     console.log(arr);
+// }
+
+// bubbleSort([1, 5, 4, 8, 6, 22, 4, 99, 7, 1, 5, 6])
